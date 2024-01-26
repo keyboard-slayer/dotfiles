cb196dca58413a2a5de3f3ca12bac75ed28b9211
keyboard-slayer <dalcjor07@gmail.com>
2024-01-26 11:48:14 +0100

    Added current dotfiles

---
+++ arch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if ! command -v git &>/dev/null
+then
+    sudo pacman -S git
+fi
+
+if ! command -v yay &>/dev/null
+then
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si
+    cd ../
+fi
+
+yay -S --needed google-chrome bemenu-wayland nodejs hyprpaper gnome-keyring \
+    xdg-desktop-portal-hyprland xdg-utils brightnessctl discord \
+    imv waybar wayshot slurp wl-clipboard blueberry pavucontrol \
+    pipewire pipewire-pulse pipewire-audio ttf-cascadia-code \
+    ttf-comic-mono-git otf-font-awesome ttf-roboto noto-fonts-emoji \
+    visual-studio-code-bin kitty hyprland stow neofetch fish neovim \
+    tmux
+
+
+systemctl --user enable --now pipewire
+systemctl --user enable --now pipewire-pulse
+
+sudo systemctl enable --now bluetooth
+
+chsh -s /usr/bin/fish
+
+mkdir ~/Pictures
+bash ./stow.sh
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    set fish_greeting
+    set -Ux EDITOR nvim
+    # Commands to run in interactive sessions can go here
+end
