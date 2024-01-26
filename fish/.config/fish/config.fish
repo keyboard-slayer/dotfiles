1006b7bf18a59c4d8faac5907a822cedd8711360
keyboard-slayer <dalcjor07@gmail.com>
2024-01-26 11:52:52 +0100

    Added scripts

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     set fish_greeting
     set -Ux EDITOR nvim
+    set -U fish_user_paths ~/.local/bin $fish_user_paths
     # Commands to run in interactive sessions can go here
 end
